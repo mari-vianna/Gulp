7c145bac1099494ec1b7f575258125aebc149ce4
Mariana Vianna <marianaavianna@gmail.com>
2022-06-06 16:21:02 -0300

    alteracoes finais em gulp styles e images

--- Projeto/gulpfile.js
+++ Projeto/gulpfile.js
@@ -3,7 +3,6 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglifly = require('gulp-uglify')
-const image = require('gulp-image')
 
 function tarefasCSS(cb){
     return gulp.src([
