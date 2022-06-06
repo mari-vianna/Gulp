b67d220f0457642c745e404dbaf305f559afc0c7
Mariana Vianna <marianaavianna@gmail.com>
2022-06-06 15:58:12 -0300

    arquivos adicionados para o exercicio

--- Projeto/gulpfile.js
+++ Projeto/gulpfile.js
@@ -3,18 +3,32 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglifly = require('gulp-uglify')
+const image = require('gulp-image')
 
 function tarefasCSS(cb){
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css', 
+        './vendor/owl/css/owl.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './vendor/jquery-ui/jquery-ui.css',
+        './src/css/style-custom.css'
+    ])
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
 }
 
 function tarefasJS(){
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/owl.carousel.js',
+        './vendor/jquery-ui/jquery-ui.js',
+        './src/js/custom.js'
+    ])
+        .pipe(concat('scripts.js'))
         .pipe(uglifly())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/js'))
